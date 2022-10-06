96e566fcadcdc731e2ae8334232d66c3911605f8
gonarhane <gonar90@gmail.com>
2022-10-06 12:28:48 +0000

    jeudi

--- pages/connection.php
+++ pages/connection.php
@@ -4,87 +4,95 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
     <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+ <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous"> 
     <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
-    <title>Document</title>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script> 
+<title>Document</title>
 </head>
 <body>
+    
+
     <div class="login">
-        <h1 class="text-center">CONNECTION</h1>
-        <form action="">
+       
+        <h1 class="text-center">Connection</h1>
+        <form action="Connection_dba.php" method="post">
             <div class="form-group">
                 <label class="form-label" for="name">Nom d'utilisateur</label>
-                <input class="form-control" type="text" id="name">
+                <input class="form-control" type="text" id="name" required>
             </div>
             <div class="form-group">
-                <label class="form-label" for="profil">profil</label>
-                <select class="form-control" name="fonction" id="fonction">
-                    <option value="administrateur">administrateur</option>
-                    <option value="secretaire">secretaire</option>
-                    <option value="professeur">professeur</option>
-                    <option value="comptable">comptable</option>
-                    <option value="eleve">eleve</option>
-                    <option value="surveillant">surveillant</option>
-                    
+                <label class="form-label" for="profil">Profil</label>
+                
+                <select class="form-control" name="fonction" id="fonction"  holderplace="fonction" required>
+                    <option value="administrateur">Administrateur</option>
+                    <option value="secretaire">Secretaire</option>
+                    <option value="professeur">Professeur</option>
+                    <option value="comptable">Comptable</option>
+                    <option value="eleve">Eleve</option>
+                    <option value="surveillant">Surveillant</option>   
                 </select>
             </div>
 
             <div class="form-group">
-                <label class="form-label" for="password">mot de passe</label>
-                <input class="form-control" type="pasword" id="passwords">
+                <label class="form-label" for="password">Mot de passe</label>
+                <input class="form-control" type="password" id="passwords" required>
             </div>
             <div class="form-group1">
-            <input class="btn btn-success w-100" type="submit" value="se connecter">
+            <input class="btn btn-success w-100" type="submit" value="Se connecter">
         </div>
         </form>
+        
     
 </body>
+<?php
+include('Connection_dba.php');
+$prenom
+?>
 <style>
     *{
-        margin:0;
-        padding: 0;
-        box-sizing:border-box;
+    margin:0;
+    padding: 0;
+    box-sizing:border-box;
 
-    }
-    body{
-        height: 110vh;
-        display: flex;
-        align-items: center;
-        justify-content:center;
-        background:#0d6efd;
-    }
-    .login{
-        width: 360px;
-        height:max-content;
-        padding:20px;
-        border-radius:12px;
-        background:#ffffff;
-    }
-    .login h1 {
-        font: size 36px;
-        margin: bottom 25px;
+}
+body{
+    height: 110vh;
+    display: flex;
+    align-items: center;
+    justify-content:center;
+    background:#0d6efd;
+}
+.login{
+    width: 360px;
+    height:max-content;
+    padding:20px;
+    border-radius:12px;
+    background:#ffffff;
+}
+.login h1 {
+    font: size 36px;
+    margin: bottom 25px;
 
-    }
-    .login form{
-        font: size 20px;
-    }
-    .login form .form-group{
-        margin: bottom 12px;
+}
+.login form{
+    font: size 20px;
+}
+.login form .form-group{
+    margin: bottom 12px;
+
+}
+.login form input[type="submit"]{
+    font-size: 20px;
+    margin: top 15px;
+}
+.form-group1{
+    margin-top: 20px;
     
-    }
-    .login form input[type="submit"]{
-        font-size: 20px;
-        margin: top 15px;
-    }
-    .form-group1{
-        margin-top: 20px;
-        
-    }
+}
 
 </style>
 
 
-   
 </html>
